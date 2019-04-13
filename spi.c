688ae79c3732e2ec15957bb16f2a69a3eb0bd922
Paul Nykiel <paul.nykiel@gmail.com>
2019-04-13 20:52:11 +0200

    Added SPI, fixed #4

--- spi.c
+++ spi.c
@@ -6,3 +6,35 @@
  */
 
 #include "spi.h"
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+volatile uint8_t *_buf;
+volatile uint16_t _size;
+
+ISR(SPI_STC_vect) {
+    _buf[-1] = SPDR;
+    if (_size > 0) {
+        SPDR = *_buf++;
+        --_size;
+    }
+}
+
+void spi_init(bool lsb_first, uint8_t f_bit) {
+    SPCR = 0b11010000; // Enabled, Interrupts enabled, Slave mode
+    if (lsb_first) {
+        SPCR |= (1 << DORD);
+    }
+    SPCR |= f_bit & 0b11;
+    SPSR = (f_bit >> 2) & 0b1;
+}
+
+void spi_tx_rx(uint8_t *buf, uint16_t size) {
+    _buf = buf;
+    _size = size;
+    if (_size > 0) {
+        SPDR = *_buf++;
+        --_size;
+    }
+}
--- spi.h
+++ spi.h
@@ -8,4 +8,17 @@
 #ifndef FLIGHTCONTROLLER_SPI_H
 #define FLIGHTCONTROLLER_SPI_H
 
+#include <stdbool.h>
+#include <stdint.h>
+
+/**
+ *
+ * @param lsb_first
+ * @param f_bit refer to page 198
+ */
+void spi_init(bool lsb_first, uint8_t f_bit);
+
+void spi_tx_rx(uint8_t *buf, uint16_t size);
+
+
 #endif //FLIGHTCONTROLLER_SPI_H
