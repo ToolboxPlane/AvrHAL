a0197373601ec36dab30368f95c20672b69b2b4d
Paul Nykiel <paul.nykiel@gmail.com>
2019-04-13 21:03:49 +0200

    Added documentation

--- spi.h
+++ spi.h
@@ -12,12 +12,18 @@
 #include <stdint.h>
 
 /**
- *
- * @param lsb_first
- * @param f_bit refer to page 198
+ * Initialize and enable the spi in master mode.
+ * @param lsb_first true if the data should be send (and received) with the least significant bit first
+ * @param f_bit the 3 bits responsible for selecting the prescaler (refer to page 198 for more information)
  */
 void spi_init(bool lsb_first, uint8_t f_bit);
 
+/**
+ * Send and receive a number of bytes via the interface. The buffer is used for reading the data to send
+ * but is also used to write the data which got read.
+ * @param buf the input-output buffer
+ * @param size the number of elements to read/write
+ */
 void spi_tx_rx(uint8_t *buf, uint16_t size);
 
 
--- uart.h
+++ uart.h
@@ -12,8 +12,27 @@
 
 typedef void (*uart_callback_t)(uint8_t);
 
+/**
+ * Initialize and enable the uart 0
+ * @param baud the baud-rate (for example 9600)
+ * @param rx_callback a functor which gets called everytime there is a new byte
+ */
 void uart0_init(uint32_t baud, uart_callback_t rx_callback);
+
+/**
+ * Send a single byte via the uart. This function manages an ring buffer (with size 32), to
+ * buffer the data befor sending. If this ring buffer is full this functions blocks until a byte
+ * had been sent.
+ * @param data the byte to send
+ */
 void uart0_send_byte(uint8_t data);
+
+/**
+ * Sends multiple bytes via the uart.
+ * @see uart0_send_byte for more information about the blocking behaviour of this function.
+ * @param data a pointer to the first element to send
+ * @param size the number of elements to send
+ */
 void uart0_send_buf(const uint8_t *data, uint16_t size);
 
 #endif //FLIGHTCONTROLLER_UART_H
