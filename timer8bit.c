bec2574463456c32d4100ba7041e43427f80fbac
Paul Nykiel <paul.nykiel@gmail.com>
2023-01-19 19:50:35 +0100

    Added timer 2 as option

--- timer8bit.c
+++ timer8bit.c
@@ -16,24 +16,59 @@ typedef struct {
     volatile uint8_t *const tccra, *const tccrb, *const timsk, *const tcnt;
 } timer8bit_instance_t;
 
-static volatile timer8bit_instance_t instance = {.tccra = &TCCR0A, .tccrb = &TCCR0B, .timsk = &TIMSK0, .tcnt = &TCNT0};
+static volatile timer8bit_instance_t instances[] = {
+        {.tccra = &TCCR0A, .tccrb = &TCCR0B, .timsk = &TIMSK0, .tcnt = &TCNT0},
+        {.tccra = &TCCR2A, .tccrb = &TCCR2B, .timsk = &TIMSK2, .tcnt = &TCNT2}};
+
+const static uint8_t prescaler_bits[2][10] = {
+        /*Timer 0:*/
+        {/*no_clock=*/0,
+         /*prescaler_1=*/1,
+         /*prescaler_8=*/2,
+         /*prescaler_32=*/0,
+         /*prescaler_64=*/3,
+         /*prescaler_128=*/0,
+         /*prescaler_256=*/4,
+         /*prescaler_1024=*/5,
+         /*external_falling*/ 6,
+         /*external_rising=*/7},
+
+        /* Timer 1: */
+        {/*no_clock=*/0,
+         /*prescaler_1=*/1,
+         /*prescaler_8=*/2,
+         /*prescaler_32=*/3,
+         /*prescaler_64=*/4,
+         /*prescaler_128=*/5,
+         /*prescaler_256=*/6,
+         /*prescaler_1024=*/7,
+         /*external_falling*/ 0,
+         /*external_rising=*/0},
+};
 
 
 ISR(TIMER0_OVF_vect) {
-    if (instance.callback != 0) {
-        (*instance.callback)();
+    if (instances[TIMER_ID_0].callback != 0) {
+        (*instances[TIMER_ID_0].callback)();
     }
 }
 
-void timer_8bit_init(timer_8bit_clock_option_t timer_clock_option, timer_8bit_callback_t callback) {
-    instance.callback = callback;
-    *instance.tccra = 0b00000000u;                      // Output compare disconnected, normal mode
-    *instance.tccrb = 0b00000000u | timer_clock_option; // No force override, normal mode, prescaler
-    *instance.timsk = 0b00000001u;                      // Overflow interrupt enabled
-
-    *instance.tcnt = 0; // Set the counter to 0
+ISR(TIMER2_OVF_vect) {
+    if (instances[TIMER_ID_2].callback != 0) {
+        (*instances[TIMER_ID_2].callback)();
+    }
 }
 
-uint8_t timer_8bit_get_count(void) {
-    return *instance.tcnt;
+void timer_8bit_init(timer_id_t num, timer_8bit_clock_options timer_clock_option, timer_8bit_callback_t callback) {
+    instances[num].callback = callback;
+    *instances[num].tccra = 0b00000000u; // Output compare disconnected, normal mode
+    *instances[num].tccrb =
+            0b00000000u | prescaler_bits[num][timer_clock_option]; // No force override, normal mode, prescaler
+    *instances[num].timsk = 0b00000001u;                           // Overflow interrupt enabled
+
+    *instances[num].tcnt = 0; // Set the counter to 0
+}
+
+uint8_t timer_8bit_get_count(timer_id_t num) {
+    return *instances[num].tcnt;
 }
--- timer8bit.h
+++ timer8bit.h
@@ -18,30 +18,47 @@ typedef void (*timer_8bit_callback_t)(void);
 
 /**
  * Possible prescaler value for the 8 bit timer.
+ *
+ * For timer 0 the following options are invalid:
+ *  * prescaler_32
+ *  * prescaler_128
+ *
+ * For timer 2 the following options are invalid:
+ *  * external_falling
+ *  * external_rising
  */
 typedef enum {
-    no_clock = 0,
-    prescaler_1 = 1,
-    prescaler_8 = 2,
-    prescaler_64 = 3,
-    prescaler_256 = 4,
-    prescaler_1024 = 5,
-    external_falling = 6,
-    external_rising = 7
-} timer_8bit_clock_option_t;
+    no_clock,
+    prescaler_1,
+    prescaler_8,
+    prescaler_32,
+    prescaler_64,
+    prescaler_128,
+    prescaler_256,
+    prescaler_1024,
+    external_falling,
+    external_rising
+} timer_8bit_clock_options;
+
+/**
+ * ID of the 8-bit timers.
+ */
+typedef enum { TIMER_ID_0 = 0, TIMER_ID_2 = 1 } timer_id_t;
 
 
 /**
  * Initialize, enable and start the timer.
+ * @param num id of the timer
  * @param timer_clock_option the prescaler
  * @param callback a functor that gets called on every overflow
  */
-void timer_8bit_init(timer_8bit_clock_option_t timer_clock_option, timer_8bit_callback_t callback);
+void timer_8bit_init(timer_id_t num, timer_8bit_clock_options timer_clock_option, timer_8bit_callback_t callback);
 
 /**
  * Read the current value of the timer
+ * @param num id of the timer
  * @return a value in [0, 255] representing the value of the timer
  */
-uint8_t timer_8bit_get_count(void);
+uint8_t timer_8bit_get_count(timer_id_t num);
 
 #endif // AVR_HAL_TIMER8BIT_H
